1d056f1e49b42bacd3de82c51425d4bb942b8f1d
user <usertemp4422@gmail.com>
2020-09-19 18:53:52 +0300

    Initial commit

---
+++ profile.ps1
@@ -0,0 +1,5 @@
+Set-Alias -name vi -value vim
+Set-Alias -name touch -value New-Item
+Set-Alias -name grep -value Select-String
+Set-Alias -name edit -value notepad
+Set-Alias -name less -value more
