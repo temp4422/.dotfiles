ab2247a306bccb8ebe1373cf6eb7886171d762c5
user <usertemp4422@gmail.com>
2020-09-28 11:16:50 +0300

    Add alias, change PS prompt.

--- profile.ps1
+++ profile.ps1
@@ -3,3 +3,4 @@ Set-Alias -name touch -value New-Item
 Set-Alias -name grep -value Select-String
 Set-Alias -name edit -value notepad
 Set-Alias -name less -value more
+function prompt {"PS $($executionContext.SessionState.Path.CurrentLocation) `n$('>' * ($nestedPromptLevel + 1)) "}
