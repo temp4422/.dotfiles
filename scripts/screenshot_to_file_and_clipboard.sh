15bf2aeaafec44602978f53d077bb25d81e84761
user <user@MacBook-Pro-user.local>
2024-12-25 07:44:39 +0200

    Add scripts/screenshot_to_file_and_clipboard.sh

---
+++ scripts/screenshot_to_file_and_clipboard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the file path for saving the screenshot
+FILENAME="$HOME/Documents/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
+
+# Take a screenshot interactively (-i) and save it to a file
+screencapture -i "$FILENAME"
+
+# Use AppleScript to copy the image to the clipboard
+osascript <<EOF
+set the clipboard to (read (POSIX file "$FILENAME") as JPEG picture)
+EOF
+
+# Generated with chatGPT 25.12.2024
