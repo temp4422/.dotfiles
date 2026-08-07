f7dda66290d879b67a7eb7024a70e6555acb9fbc
user <user@MacBook-Pro-user.local>
2026-08-07 16:35:19 +0300

    WIP

--- src/karabiner-elements-generate.js
+++ src/karabiner-elements-generate.js
@@ -76,6 +76,8 @@ const manipulators = [
     .setVar('copyModeSpace', 0),
 
   // Navigation (Copy Mode)
+
+  // hoome/end/page_up/page_down
   mapKey('m', 'right_command')
     .ifVar('copyMode', 1)
     .desc('Home -> 0')
@@ -88,19 +90,136 @@ const manipulators = [
     .to('4', 'left_shift')
     .setVar('copyModeSpace', 0),
 
+  // jkl;
+  mapKey('j', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Move to left')
+    .to('left_arrow'),
+
+  mapKey('j', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Move to left')
+    .to('spacebar')
+    .to('left_arrow')
+    .setVar('copyModeSpace', 0),
+
+  mapKey('k', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Move to down')
+    .to('down_arrow'),
+
+  mapKey('k', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Move to down')
+    .to('spacebar')
+    .to('down_arrow')
+    .setVar('copyModeSpace', 0),
+
+  mapKey('l', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Move to up')
+    .to('up_arrow'),
+
+  mapKey('l', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Move to up')
+    .to('spacebar')
+    .to('up_arrow')
+    .setVar('copyModeSpace', 0),
+
+  mapKey('semicolon', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Move to right')
+    .to('right_arrow'),
+
+  mapKey('semicolon', ['right_command'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Move to right')
+    .to('spacebar')
+    .to('right_arrow')
+    .setVar('copyModeSpace', 0),
+
   // Selection (Copy Mode)
+
+  // hoome/end/page_up/page_down
   mapKey('m', ['right_command', 'left_shift'])
     .ifVar('copyMode', 1)
     .desc('Select to Home')
     .to('spacebar')
-    .to('0'),
+    .to('left_arrow', 'left_command'),
 
   mapKey('slash', ['right_command', 'left_shift'])
     .ifVar('copyMode', 1)
     .desc('Select to End')
     .to('spacebar')
-    .to('4', 'left_shift'),
+    .to('right_arrow', 'left_command'),
 
+  // jkl;
+  mapKey('j', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Select to left')
+    .to('spacebar')
+    .to('left_arrow')
+    .setVar('copyModeSpace', 1),
+
+  mapKey('j', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Select to left in copyModeSpace')
+    .to('left_arrow'),
+
+  mapKey('k', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Select to down')
+    .to('spacebar')
+    .to('down_arrow')
+    .setVar('copyModeSpace', 1),
+
+  mapKey('k', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Select to down in copyModeSpace')
+    .to('down_arrow'),
+
+  mapKey('l', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Select to up')
+    .to('spacebar')
+    .to('up_arrow')
+    .setVar('copyModeSpace', 1),
+
+  mapKey('l', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Select to up in copyModeSpace')
+    .to('up_arrow'),
+
+  mapKey('semicolon', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 0)
+    .desc('Select to right')
+    .to('spacebar')
+    .to('right_arrow')
+    .setVar('copyModeSpace', 1),
+
+  mapKey('semicolon', ['right_command', 'left_shift'])
+    .ifVar('copyMode', 1)
+    .ifVar('copyModeSpace', 1)
+    .desc('Select to right in copyModeSpace')
+    .to('right_arrow'),
+
+  // select world
   mapKey('d', 'left_command')
     .ifVar('copyMode', 1)
     .desc('Select word')
@@ -124,7 +243,7 @@ const karabinerConfig = {
   description: 'Generated complex modifications',
   rules: [
     {
-      description: '🔴 iTerm2 Overwrite',
+      description: '🔴 iTerm2 Overwrite GENERATED',
       manipulators: manipulators,
     },
   ],
