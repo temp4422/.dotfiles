36092bc3515681028baa0c7e716445e2a485bf78
user <user@MacBook-Pro-user.local>
2026-08-14 00:07:25 +0300

    WezTerm make CMD behave like CTRL by default

--- src/wezterm/keybinds.lua
+++ src/wezterm/keybinds.lua
@@ -3,10 +3,35 @@ local wezterm = require 'wezterm'
 local module = {}
 
 function module.apply_to_config(config)
+  -- Make CMD behave like CTRL by default
+  config.keys = {}
+  local alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  for i = 1, #alphabet do
+    local char = string.sub(alphabet, i, i)
+    table.insert(config.keys, {
+      key = char,
+      mods = 'CMD',
+      action = wezterm.action.SendKey { key = char, mods = 'CTRL' },
+    })
+    table.insert(config.keys, {
+      key = char,
+      mods = 'CMD|SHIFT',
+      action = wezterm.action.SendKey { key = char, mods = 'CTRL|SHIFT' },
+    })
+  end
+
   -- Set my custom keybinds
-  config.keys = {
+  local my_custom_keys = {
+    -- Below keybinds are commented out because they are already set above with CMD -> CTRL mapping
     -- Interrupt
-    { key = 'c',          mods = 'CMD',       action = wezterm.action.SendKey { key = 'c', mods = 'CTRL' } },
+    -- { key = 'c',          mods = 'CMD',       action = wezterm.action.SendKey { key = 'c', mods = 'CTRL' } },
+    -- Select all
+    -- { key = 'a',          mods = 'CMD',       action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' } },
+    -- Undo
+    -- { key = 'z',          mods = 'CMD',       action = wezterm.action.SendKey { key = 'z', mods = 'CTRL' } },
+    -- Clear scrollback and viewport
+    -- { key = 'k',          mods = 'CMD',       action = wezterm.action.ClearScrollback 'ScrollbackAndViewport' },
+
     -- Split pane horizontally
     { key = 'd',          mods = 'CMD|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     -- Close pane
@@ -14,11 +39,7 @@ function module.apply_to_config(config)
     -- Select pane
     { key = 'LeftArrow',  mods = 'CMD|OPT',   action = wezterm.action.ActivatePaneDirection 'Left' },
     { key = 'RightArrow', mods = 'CMD|OPT',   action = wezterm.action.ActivatePaneDirection 'Right' },
-    -- Clear scrollback and viewport
-    { key = 'k',          mods = 'CMD',       action = wezterm.action.ClearScrollback 'ScrollbackAndViewport' },
 
-    -- Undo
-    { key = 'z',          mods = 'CMD',       action = wezterm.action.SendKey { key = 'z', mods = 'CTRL' } },
     -- Redo
     { key = 'z',          mods = 'CMD|SHIFT', action = wezterm.action.SendKey { key = 'z', mods = 'OPT' } },
     -- action = wezterm.action.SendString("\x1bz")
@@ -27,13 +48,17 @@ function module.apply_to_config(config)
     -- https://wezterm.org/config/keys.html#configuring-key-assignments
     -- https://wezterm.org/config/lua/keyassignment/SendString.html
 
-    -- Select all
-    { key = 'a',          mods = 'CMD',       action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' } },
 
+    --TODO
     -- Move to beginning/end of line
     -- { key = 'LeftArrow',  mods = 'OPT',       action = wezterm.action.SendString("\x1b[H") },
     -- { key = 'LeftArrow',  mods = 'OPT',       action = wezterm.action.SendString("\27[H") },
   }
+
+  -- Add my custom keybinds to the config
+  for _, keybinding in ipairs(my_custom_keys) do
+    table.insert(config.keys, keybinding)
+  end
 end
 
 return module
